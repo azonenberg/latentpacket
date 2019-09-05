75e3dd759627292a88053b4d6601b7e78dba6896
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-09-05 06:48:08 -0700

    Added RxFifoBus forgotten in previous commit

---
+++ latentred/rtl/fabric/RxFifoBus.svh
@@ -0,0 +1,45 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* LATENTPACKET v0.1                                                                                                    *
+*                                                                                                                      *
+* Copyright (c) 2018-2019 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+`ifndef RxFifoBus_h
+`define RxFifoBus_h
+
+typedef struct packed
+{
+	logic				frame_valid;		//When high, there is a frame ready to forward
+	logic[47:0]			frame_dst_mac;		//Destination of the frame
+	logic[47:0]			frame_src_mac;		//Source of the frame
+	logic[11:0]			frame_vlan;			//VLAN ID of the frame
+	logic				fwd_valid;			//High if data being forwarded is valid
+	logic[3:0]			fwd_bytes_valid;	//Number of valid bytes in this 64-bit block
+											//(always 8 except in last one)
+	logic[63:0]			fwd_data;			//The actual data being forwarded through the fabric
+} RxFifoBus;
+
+`endif
